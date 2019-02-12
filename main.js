7d482e7489effa3818ac935ca695ebd59408552d
joanna-by <j.bykowska@gmail.com>
2019-02-12 18:26:32 +0100

    the base of a webpage

---
+++ main.js
@@ -0,0 +1,14 @@
+var answers = [
+    "Definitely yes",
+    "That's a great idea",
+    "Just do it",
+    "I am not so sure",
+    "Think it over one more time",
+    "Rather not",
+    "The computer says no",
+]
+
+var findAnswer = answers[Math.floor(Math.random() * 7)];
+
+console.log(findAnswer);
+
